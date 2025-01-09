a0a6cd68895b9579c680fd0fd0635a1cc73dc795
darryl1975 <darryl1975@hotmail.com>
2025-01-09 11:44:33 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/vttp5b_paf_day21/Vttp5bPafDay21Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5b_paf_day21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5bPafDay21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5bPafDay21Application.class, args);
+	}
+
+}
